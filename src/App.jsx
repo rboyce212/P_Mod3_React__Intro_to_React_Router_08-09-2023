4d3bafcc7d9c1f75e162ead47261adba8fc8585c
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-08-11 12:05:40 -0400

    REact App Started, Boiler-Plate Code removed; installed React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import './App.css'
-
-import cohortArray from "./data/cohortArray.json"
-
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
-
-  console.log("cohortArray: ", cohortArray)
-  // const [v, f]
-
-  const fellowsToRender = cohortArray.map( 
-    (eachFellowObj)=>{  
-      return(<div>
-        <h4>Name: {eachFellowObj.name}</h4>
-      </div>)  
-    } 
-  )
-
-
-  // Handling Duplicate Months
-  const filteredMonths = []
-  
-    cohortArray.forEach(  
-      (eachFellowObject)=>{
-
-        // June July October March
-        if(eachFellowObject.birthdayMonth === "July"){
-          filteredMonths.push("July")
-        }
-      } 
-  )
-  console.log("filteredMonths: ", filteredMonths)
-
-  const monthstoRender = filteredMonths.map( (eachMonthString) => <h2>{eachMonthString}</h2>    )
-
-
-
-
   return (
     <>
-      <div id="logoz">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Help With Click Filters</h1>
-
-
-      <div className='for-two-colums'>
-
-        <div className='birthday-month-list'>
-          <h2>Birthday Months</h2>
-          {monthstoRender}
-        </div>
-
-
-        <div className='fellows'>
-          <h2>Our Amazing/Lovely Fellows✨</h2>
-          {fellowsToRender}
-        </div>
-        
-      </div>
-
-
+      <Router>
+        <h1>Screenviews</h1>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
